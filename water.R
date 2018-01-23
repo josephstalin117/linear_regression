a3b045d6215d762a82d0ac6a6eb321de6c1f5658
josephlin <josephlin117@gmail.com>
2018-01-23 20:17:34 +0800

    add NEW file to ols

---
+++ water.R
@@ -0,0 +1,57 @@
+
+#read file
+beijing<-read.csv("F:/Projects/linear_regression/beijing.csv",header=TRUE)
+tianjin<-read.csv("F:/Projects/linear_regression/tianjin.csv",header=TRUE)
+hebei<-read.csv("F:/Projects/linear_regression/hebei.csv",header=TRUE)
+
+cols<-colnames(beijing)
+other_factor_manage_list<-c()
+other_factor_nature_list<-c()
+water_quality_list<-c()
+emissions_list<-c()
+
+for(i in 1:length(cols)){
+  if(i<11){
+    other_factor_nature_list<-c(other_factor_nature_list,cols[i])
+  }else if(i>=11 && i<16){
+    water_quality_list<-c(water_quality_list,cols[i])
+  }else if(i>=16 && i<30){
+    emissions_list<-c(emissions_list,cols[i])
+  }else{
+    other_factor_manage_list<-c(other_factor_manage_list,cols[i])
+  }
+  
+}
+
+#print(other_factor_manage_list)
+#print(other_factor_nature_list)
+#print(water_quality_list)
+#print(emissions_list)
+
+interaction_ols<-function(water_qualitys,emissions,other_factors,data_source){
+  lm_list<-c()
+  
+  for(water_item in water_qualitys){
+    for(emissions_item in emissions){
+      for(other_factor_item in other_factors){
+        #print(water_item)
+        #print(emissions_item)
+        #print(other_factor_item)
+        
+        xnam<-c(emissions_item,other_factor_item)
+        f<-as.formula(paste(paste(water_qualitys," ~ "),paste(xnam,collapse = "*")))
+        
+        #lm_fit<-lm(as.formula(paste(paste(water_qualitys," ~ "),paste(xnam,collapse = "*"))),data=data_source)
+        #lm_list<-c(lm_list,lm_fit)
+      }
+    }
+  }
+  return(lm_list)
+}
+
+lm_list=interaction_ols(water_quality_list,emissions_list,other_factor_manage_list,beijing)
+
+#lm_item<-lm(COD~1+wastewater_total_discharge*industrial_wastewater_treatment_facilities,data=beijing)
+#summary(lm_item)
+
+#summary(lm_beijing)
