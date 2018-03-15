34c68e72ab214ea9639dc19d7aa599e4625546ee
josephlin <josephlin117@gmail.com>
2018-03-15 20:56:01 +0800

    add data cleaning function

--- logistic_regression.py
+++ logistic_regression.py
@@ -22,39 +22,21 @@ for province in provinces:
     df_dict[province].reset_index(inplace=True,drop=True)
 
 ##data cleaning
-
-#test_df just use to test
-    
-#test_df=df_dict['beijing'].dropna(axis=1,how='any')
-test_df=df_dict['beijing']
-
-#print(test_df['province'].dtype)
-#print(test_df['province'].dtype=='object')
-
-#unified unit
-for column in test_df:
-    if test_df[column].dtype!='object' and len(str(int(test_df[column].max())))-len(str(int(test_df[column].min())))>=3:
-        for item in test_df[column]:
-            if str(item)!='nan' and len(str(int(test_df[column].max())))-len(str(int(item)))>=3:
-                #print(test_df[column][item])
-                #print(item)
-                test_df.loc[test_df[column] == item, column] = item*10000
-
-#remove or fitting data
-for column in test_df:
-    #print(test_df[column].isnull().sum())
-    if test_df[column].isnull().sum()<2 and test_df[column].dtype!='object':
-        test_df[column]=test_df[column].fillna(test_df[column].mean())
-
-test_df=test_df.dropna(axis=1,how='any')
    
-#jianchi!
-for province in provinces:
-    for column in dict[province]:
-        if dict[province][column].dtype!='object' and len(str(int(dict[province][column].max())))-len(str(int(dict[province][column].min())))>=3:
-            for item in
+def data_cleaning(df):
+    #unified units
+    for col in df:
+        if df[col].dtype!='object' and str(df[col].min())!='nan' and len(str(int(df[col].max())))-len(str(int(df[col].min())))>=3:
+            for item in df[col]:
+                if str(item)!='nan' and len(str(int(df[col].max())))-len(str(int(item)))>=3:
+                    df.loc[df[col]==item,col]=item*10000
+    #remove or fitting data
+    for col in df:
+        if df[col].isnull().sum()<=2 and df[col].dtype!='object':
+            df[col]=df[col].fillna(df[col].mean())
+    df=df.dropna(axis=1,how='any')
+    return df
 
-            
-#df_dict[province]=df_dict[province].dropna(axis=1,thresh=3)
-    
-    
+
+for province in provinces:
+    df_dict[province]=data_cleaning(df_dict[province])
