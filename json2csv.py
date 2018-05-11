00b7fabb6bde1a39d1a86b1e502873ad593826eb
josephlin <josephlin117@gmail.com>
2018-05-11 15:03:45 +0800

    convert json to pd

--- json2csv.py
+++ json2csv.py
@@ -1,14 +1,17 @@
 # -*- coding:utf8 -*-
-import csv
-import codecs
-import json
 
-with open('waterdata_5_5.txt', encoding='utf-8') as f:      
+import json
+import pandas as pd
+
+sourcefile="waterdata_5_5.txt"
+csvfilename="waterdata_5_5.csv"
+df = pd.DataFrame(columns=['dateTime','CODMn', 'DO', 'NH4', 'TOC', 'attribute','level','pH','siteName','status'])
+
+with open(sourcefile, encoding='utf-8') as f:      
     for r in f.readlines():
-        data=json.loads(json.dumps(r))
         s=r.split('}')[:-1]
         for i in s:
             i=i+'}'
             i=i.replace("'", '"')
-            #json_i=json.dumps(i,ensure_ascii=False)
             data_dict=json.loads(i)
+            df=df.append(data_dict,ignore_index=True)
